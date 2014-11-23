000a93ba1763de8de0136dd75ff035325e73e11e
achtor <peterx@uchicago.edu>
2014-11-22 18:39:36 -0600

    Some preliminary builds.

---
+++ main.py
@@ -0,0 +1,23 @@
+import traceback, sys, re
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def root():
+   return render_template('index.html')
+
+@app.route('/subscribe/', methods=['GET', 'POST'])
+def subscribe():
+   address = request.form['address']
+   email = request.form['email']
+   e_wf  = re.match(r"[^@]+@[^@]+\.[^@]+",email)
+   a_wf = (address != "")
+   if e_wf and a_wf:
+      do stuff
+   else:
+      return render_template('index.html', msg = "Please enter a valid email and address.")
+
+if __name__ == '__main__':
+   app.run()
